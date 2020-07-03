6aab2e773fcb437d87fd8766fae13ed8dffdb252
Kevin van der Kruk <kevin.vanderkruk@gmail.com>
2020-07-03 11:36:44 +0200

    String counts working excersize 1

--- src/main/java/StringTest.java
+++ src/main/java/StringTest.java
@@ -2,16 +2,31 @@ public class StringTest {
 
     public static void main(String[] args) {
 
-        int count = 0;
+        int countA = 0;
+        int countB = 0;
+        int countC = 0;
+
         String a = "Lepel";
         String b = "http://www.smartdeveloper.be";
         String c = "De mooie zeeman nam Anne mee zei oom Ed";
 
         for(int i = 0; i < a.length(); i++) {
             if(a.charAt(i) != ' ')
-                count++;
+                countA++;
         }
-        System.out.println("Total number of characters in the String: " + a + " equals: " + count);
+        System.out.println("Total number of characters in the String: " + a + " equals: " + countA);
+
+        for(int i = 0; i < b.length(); i++) {
+            if(b.charAt(i) != ' ')
+                countB++;
+        }
+        System.out.println("Total number of characters in the String: " + b + " equals: " + countB);
+
+        for(int i = 0; i < b.length(); i++) {
+            if(c.charAt(i) != ' ')
+                countC++;
+        }
+        System.out.println("Total number of characters in the String: " + c + " equals: " + countC);
 
 
     }
