f3e5bbb631e395145926f97aa68a18ed082305e4
Kevin van der Kruk <kevin.vanderkruk@gmail.com>
2020-07-03 12:12:31 +0200

    vowel count completed

--- src/main/java/StringTest.java
+++ src/main/java/StringTest.java
@@ -3,34 +3,41 @@ public class StringTest {
     public static void main(String[] args) {
 
         int countA = 0;
+        int vowelsA = 0;
         int countB = 0;
         int countC = 0;
+
 
         String a = "Lepel";
         String b = "http://www.smartdeveloper.be";
         String c = "De mooie zeeman nam Anne mee zei oom Ed";
 
-        for(int i = 0; i < a.length(); i++) {
-            if(a.charAt(i) != ' ')
-                countA++;
+        a = a.toLowerCase();
+
+        for (int i = 0; i < a.length(); i++) {
+            char ch = a.charAt(i);
+            if (ch == 'a' || ch == 'e' || ch == 'i'
+                    || ch == 'o' || ch == 'u') {
+                ++vowelsA;
+            }
+
+            System.out.println("Total number of vowels in String: " + a + " equals: " + countA);
+            System.out.println();
+
+            for (int j = 0; j < b.length(); j++) {
+                if (b.charAt(j) != ' ')
+                    countB++;
+            }
+            System.out.println("Total number of characters in String: " + b + " equals: " + countB);
+            System.out.println("Is this URL valid: " + !ValidateUrl.validateURL(b));
+
+            for (int k = 0; k < b.length(); k++) {
+                if (c.charAt(k) != ' ')
+                    countC++;
+            }
+            System.out.println("Total number of characters in the String: " + c + " equals: " + countC);
+
+
         }
-        System.out.println("Total number of characters in the String: " + a + " equals: " + countA);
-
-
-        for(int i = 0; i < b.length(); i++) {
-            if(b.charAt(i) != ' ')
-                countB++;
-        }
-        System.out.println("Total number of characters in the String: " + b + " equals: " + countB);
-        System.out.println("Is this URL valid: " + !ValidateUrl.validateURL(b));
-
-        for(int i = 0; i < b.length(); i++) {
-            if(c.charAt(i) != ' ')
-                countC++;
-        }
-        System.out.println("Total number of characters in the String: " + c + " equals: " + countC);
-
-
-
     }
 }
